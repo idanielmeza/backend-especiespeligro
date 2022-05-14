34717edc59db897829045144b51a95fb9b20a536
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-14 08:09:33 -0700

    unit test for especie controller

---
+++ test/controllers/especie.test.js
@@ -0,0 +1,49 @@
+const { getEspecies, getEspecie, postEspecie, putEspecie } = require("../../controllers/especie");
+
+describe("Test para controlador especie", () => {
+    const mockRequest = (sessionData) => {
+        return {
+            session: { data: sessionData}
+        }
+    }
+
+    const mockResponse = () => {
+        const res = {};
+        res.status = jest.fn().mockReturnValue(res)
+        res.json = jest.fn().mockReturnValue(res)
+        return res;
+    }
+    
+    test("1. Error en la función getEspecies", async () => {
+      const req = mockRequest();
+      const res = mockResponse();
+      await getEspecies(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ msg: 'Hubo un error' })
+    });
+
+    test("2. Error en la función getEspecie", async () => {
+        const req = mockRequest();
+        const res = mockResponse();
+        await getEspecie(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ msg: 'Hubo un error' })
+    });
+
+    test("3. Error en la función postEspecie", async () => {
+        const req = mockRequest();
+        const res = mockResponse();
+        await postEspecie(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ msg: 'Hubo un error' })
+    });
+
+    test("4. Error en la función putEspecie", async () => {
+        const req = mockRequest();
+        const res = mockResponse();
+        await putEspecie(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+        expect(res.json).toHaveBeenCalledWith({ msg: 'Hubo un error' })
+    });
+
+});
