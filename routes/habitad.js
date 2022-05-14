ac635aa396ce86819af9333642100e9ff6a2b15c
Alejandro Garcia <v4gr33n@hotmail.com>
2022-05-13 23:12:29 -0500

    Agrega routes/habitad endpoints

--- routes/habitad.js
+++ routes/habitad.js
@@ -1,5 +1,18 @@
 const {Router} = require('express');
+const {check} = require('express-validator');
+const {validarCampos} = require('../middlewares/validar-campos');
+
 
 const router = Router();
+
+router.post('/', [
+    check('habitad','El habitad es obligatorio').not().isEmpty().trim().escape(),
+    validarCampos
+]);
+
+router.put('/:id', [
+    check('id', 'El id es obligatorio').isMongoId(),
+    validarCampos
+]);
 
 module.exports = router;
