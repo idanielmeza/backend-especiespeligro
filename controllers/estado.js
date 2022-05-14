9b7ba9bd3466911315c366df9709dcaaa4b6d7dd
idanielmeza <idanielmezaledezma@gmail.com>
2022-05-14 10:21:15 -0500

    Ruta de estado agregada

---
+++ controllers/estado.js
@@ -0,0 +1,54 @@
+const Estado = require('../models/estado');
+
+const getEstados = async(req,res) => {
+    try {
+        const [total, estados] = await Promise.all([
+            Estado.countDocuments(),
+            Estado.find()
+            .skip(Number(desde))
+            .limit(Number(limite))
+        ])
+        res.status(200).json({
+            total,
+            estados
+        });
+    } catch (error){
+        res.status(500).json({msg: 'Hubo un error'});
+    }
+}
+
+const getEstado = async(req,res) => {
+    try {
+        const estado = await Estado.findById(req.params.id);
+        req.status(200).json(Estado);
+    } catch (error){
+        res.status(500).json({msg: 'Hubo un error'});
+    }
+}
+
+const postEstado = async(req,res) => {
+    try {
+        const estado = new Estado(req.body);
+        await estado.save();
+        res.status(201).json(Estado);
+    } catch (error){
+        res.status(500).json({msg: 'Hubo un error'});
+    }
+}
+
+const putEstado = async(req,res) => {
+    try {
+        const estado = await Estado.findByIdAndUpdate(req.params.id, req.body);
+        res.status(200).json(estado);
+    } catch (error){
+        res.status(500).json({msg: 'Hubo un error'});
+    }
+}
+
+module.exports = {
+    getEstado,
+    getEstados,
+    postEstado,
+    putEstado
+}
+
