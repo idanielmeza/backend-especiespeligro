86b3eea73754c1ddf90b9d3328ab53d91ddfc360
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2022-05-13 23:19:24 -0700

    subirArchivo test 1

---
+++ test/helpers/subir-archivo.test.js
@@ -0,0 +1,15 @@
+const { subirArchivo } = require("../../helpers/subir-archivo");
+
+describe("Test para subir archivos", () => {
+  test("1. Extensión inválida", async () => {
+    const file = {
+      archivo: {
+        name: "si.jpggg",
+      },
+    };
+
+    await expect(subirArchivo(file, undefined, "images")).rejects.toEqual(
+      "Tipo de archivo invalido"
+    );
+  });
+});
